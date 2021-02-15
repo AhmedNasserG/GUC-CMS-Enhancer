b85ca5aea24c3aab47cc8b71e843d0993258cb8b
Ahmed Nasser <ahmednasser217217@gmail.com>
2021-02-15 18:09:33 +0200

    Rename downloaded files

--- content.js
+++ content.js
@@ -1,4 +1,8 @@
-var button = document.getElementsByTagName('a');
-for (var i = 0, l = button.length; i < l; i++) {
-  button[i].setAttribute('style', 'display:yes;');
+var card = document.getElementsByClassName('card mb-4');
+for (var i = 1, l = card.length; i < l; i++) {
+    var name = card[i].getElementsByTagName('strong')[0].innerText;
+    var button = card[i].getElementsByTagName('a')[0];
+    button.setAttribute('style', 'display:yes;')
+    button.setAttribute('download', name);
+
 }
