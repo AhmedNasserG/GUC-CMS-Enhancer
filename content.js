e4d010cc0cdfdf402e8073542bf6689c48d3ef5d
Ahmed Nasser <ahmednasser217217@gmail.com>
2021-03-22 19:09:18 +0200

    Fix: Renaming the downloaded files

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 var card = document.getElementsByClassName('card mb-4');
 for (var i = 1, l = card.length; i < l; i++) {
-    var name = card[i].getElementsByTagName('strong')[0].innerText;
+    var name = card[i].getElementsByTagName('strong')[0].innerText.replace(/^\d.*-\s/, '');
     var button = card[i].getElementsByTagName('a')[0];
     button.setAttribute('style', 'display:yes;')
     button.setAttribute('download', name);
