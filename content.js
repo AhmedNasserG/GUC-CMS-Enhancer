ec198711766a4d5ed2bb4587a28b8471cec3c9b5
Ahmed Nasser <ahmednasser217217@gmail.com>
2021-03-22 19:15:59 +0200

    Fix: the first file not working

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 var card = document.getElementsByClassName('card mb-4');
-for (var i = 1, l = card.length; i < l; i++) {
+for (var i = 0, l = card.length; i < l; i++) {
     var name = card[i].getElementsByTagName('strong')[0].innerText.replace(/^\d.*-\s/, '');
     var button = card[i].getElementsByTagName('a')[0];
     button.setAttribute('style', 'display:yes;')
