65ea14c2d693ee3eb255096be8be8d903bb0c5b2
Ahmed Nasser <ahmednasser217217@gmail.com>
2021-03-23 14:49:50 +0200

    Fix: regex

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 var card = document.getElementsByClassName('card mb-4');
 for (var i = 0, l = card.length; i < l; i++) {
-    var name = card[i].getElementsByTagName('strong')[0].innerText.replace(/^\d.*-\s/, '');
+    var name = card[i].getElementsByTagName('strong')[0].innerText.replace(/^\d\d?\s-\s/, '');
     var button = card[i].getElementsByTagName('a')[0];
     button.setAttribute('style', 'display:yes;')
     button.setAttribute('download', name);
